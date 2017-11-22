10c132a102abd2c92dfc3a6b1b890a23839e1fb2
Ugur <ugur@connectholland.nl>
2017-11-22 14:32:36 +0100

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "approot",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'app works!';
+export class appComponent {
+  title: string = "app works!";
 }
