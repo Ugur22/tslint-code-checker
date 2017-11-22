d6b067b1a54382b18fcde55a834cbb0e35cb76aa
Ugur <ugur@connectholland.nl>
2017-11-22 16:39:41 +0100

    real fix yes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'approot',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class appComponent {
   title = 'app works!';
 }
