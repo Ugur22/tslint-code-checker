d915d7df8cf0d0b52347c92047d702f91be8a119
Ugur <ugur@connectholland.nl>
2017-11-22 16:47:57 +0100

    real fix homes

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'apphome',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class homeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
